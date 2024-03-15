ea8e1300ec7c5fb8e624332ef2098a9263d8dd7e
‘Jose <joseale282003@gmail.com>
2024-03-14 17:30:10 -0700

    horas extras

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ document.getElementById('formSalario').addEventListener('submit', function(event
     event.preventDefault();
     var horas = document.getElementById('horas').value;
     var tarifa = document.getElementById('tarifa').value;
-    var salario = horas * tarifa;
-    document.getElementById('resultado').textContent = 'El salario es: ' + salario;
+    var totalPago = horas * tarifa;
+
+    if(horas>40){
+        totalHorasExtra = horas-40;
+        totalHoras = horas-totalHorasExtra;
+        totalPago = (totalHoras*tarifa)+(totalHorasExtra*tarifa*1.5);
+        document.getElementById('resultado').textContent = 'El salario es: ' + totalPago;
+    } else {
+        totalPago = horas * tarifa;
+        document.getElementById('resultado').textContent = 'El salario es: ' + totalPago;
+    }   
+    
 });
+
+    
